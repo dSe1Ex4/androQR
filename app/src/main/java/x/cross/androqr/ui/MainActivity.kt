aad14f12bc6bc7bbbb9e7357d7c91ae9bc465f0a
dSe1Ex4 <starg99@mail.ru>
2021-03-01 14:44:52 +0500

    Создание UI
    + Создана активити загрузка (Splash)
    + Главное активити с QR сканером
    + Подключены библиотеки

---
+++ app/src/main/java/x/cross/androqr/ui/MainActivity.kt
@@ -0,0 +1,85 @@
+package x.cross.androqr.ui
+
+import android.Manifest
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.budiyev.android.codescanner.*
+import pub.devrel.easypermissions.AfterPermissionGranted
+import pub.devrel.easypermissions.EasyPermissions
+import x.cross.androqr.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
+    companion object{
+        private const val RQ_CODE_CAMERA = 1
+    }
+
+    private lateinit var view: ActivityMainBinding
+    private lateinit var codeScanner: CodeScanner
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        view = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(view.root)
+
+        openQRScanner()
+    }
+
+    @AfterPermissionGranted(RQ_CODE_CAMERA)
+    private fun openQRScanner(){
+        //Есть ли разрешение
+        if (EasyPermissions.hasPermissions(this, Manifest.permission.CAMERA)) {
+            codeScanner = CodeScanner(this, view.scannerView)
+
+            // Parameters (default values)
+            codeScanner.camera = CodeScanner.CAMERA_BACK // or CAMERA_FRONT or specific camera id
+            codeScanner.formats = CodeScanner.ALL_FORMATS // list of type BarcodeFormat,
+            // ex. listOf(BarcodeFormat.QR_CODE)
+            codeScanner.autoFocusMode = AutoFocusMode.SAFE // or CONTINUOUS
+            codeScanner.scanMode = ScanMode.SINGLE // or CONTINUOUS or PREVIEW
+            codeScanner.isAutoFocusEnabled = true // Whether to enable auto focus or not
+            codeScanner.isFlashEnabled = false // Whether to enable flash or not
+
+            // Код отсканирован
+            codeScanner.decodeCallback = DecodeCallback {
+                runOnUiThread {
+                    Toast.makeText(this, "Scan result: ${it.text}", Toast.LENGTH_LONG).show()
+                }
+            }
+
+            // Ошибка инициализации камеры
+            codeScanner.errorCallback = ErrorCallback {
+                runOnUiThread {
+                    Toast.makeText(this, "Camera initialization error: ${it.message}",
+                            Toast.LENGTH_LONG).show()
+                }
+            }
+
+            view.scannerView.setOnClickListener {
+                codeScanner.startPreview()
+            }
+        } else { //Нет разрешения на камеру
+            EasyPermissions.requestPermissions(this, "Права суука дал живо!",
+                    RQ_CODE_CAMERA, Manifest.permission.CAMERA)
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this)
+    }
+
+
+    override fun onResume() {
+        super.onResume()
+        codeScanner.startPreview()
+    }
+
+    override fun onPause() {
+        codeScanner.releaseResources()
+        super.onPause()
+    }
+}
---
+++ app/src/main/java/x/cross/androqr/ui/SplashActivity.kt
@@ -0,0 +1,70 @@
+package x.cross.androqr.ui
+
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import com.nikitagordia.cosin.textAdapters.DefaultBinaryTextAdapter
+import kotlinx.coroutines.*
+import x.cross.androqr.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+    companion object{
+        private const val TIME_FOR_LOAD = 2_500L
+    }
+
+
+    private val activityScope = CoroutineScope(Dispatchers.Main)
+    private lateinit var view: ActivitySplashBinding
+    private lateinit var mainIntent: Intent
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        view = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(view.root)
+
+        supportActionBar?.hide()
+
+        setFullScreen()
+
+        with(view.splashAnim){
+            textAdapter = DefaultBinaryTextAdapter()
+            speed = 0.005
+            rectWidth = 40
+            isLoadingData = true
+        }
+
+        mainIntent = Intent(this, MainActivity::class.java)
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        activityScope.launch {
+            delay(TIME_FOR_LOAD)
+
+            startActivity(mainIntent)
+            finish()
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        activityScope.cancel()
+    }
+
+    private fun setFullScreen(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+    }
+}
