c6a7b65f78545304247254b841ef2ec44d3bdf61
ivoylova <ivojlovaelizaveta1@gmail.com>
2021-03-07 14:13:26 +0500

    Добавление AuthActivity

---
+++ app/src/main/java/x/cross/androqr/ui/AuthActivity.kt
@@ -0,0 +1,10 @@
+package x.cross.androqr.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AuthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
