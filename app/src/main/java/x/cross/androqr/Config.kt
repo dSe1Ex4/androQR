4fb06a5580ca242a9f0de4ac108b1d1a696f4498
Sergey_Artemiev <sergArtmiev0024@yandex.ru>
2021-03-28 16:17:26 +0500

    Begining code documentation

--- app/src/main/java/x/cross/androqr/Config.kt
+++ app/src/main/java/x/cross/androqr/Config.kt
@@ -4,6 +4,12 @@ package x.cross.androqr
  * Объект глобальной конфигурации проекта
  */
 object Config {
+    /**
+     *Это дебаг
+     */
     const val DEBUG = true
+    /**
+     * Базавая ссылка Rest_api
+     */
     const val REST_API_BASE_URL = "http://10.0.2.2:8081"
 }
